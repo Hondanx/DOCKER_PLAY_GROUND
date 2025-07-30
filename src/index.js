63eaa18d75243d0d697c597c28c16d29b78a8f2a
Hondanx <mohanedmohamed6000@gmail.com>
2025-07-30 15:53:24 +0300

    aidsmongo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 
 // Init app
 const PORT = process.env.PORT || 4000;
 const app = express();
+
+// Connect to MongoDB
+const DB_USER = 'root';
+const DB_PASSWORD = '1923';
+const DB_PORT = 27017;
+const DB_HOST = 'mongo'; // Change to your MongoDB service name
+
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+mongoose
+  .connect(URI)
+  .then(() => console.log('connect to db ...'))
+  .catch((err) => console.log('failed to connect to db: ', err));
 
 app.get('/', (req, res) => res.send('<h1> Hello Tresmerge! hi </h1>'));
 
